7c6171f814bcc754496cf03e7631e1b47a44d26c
Luis Montealegre <montealegreluis@gmail.com>
2017-05-08 11:32:32 -0500

    Add hello world example.

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    // psvm
+    public static void main(String[] args) {
+        // sout
+        /*
+         * multiple lines
+         * comment
+         */
+        System.out.println("Hello Pinnacles!"); // Inline comments
+        {
+            /* single line comments */
+            System.out.println("This is valid in Java");
+        }
+    }
+}
