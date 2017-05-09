153a6f1acb98dd680aaff900c136eb7c843ce784
Luis Montealegre <montealegreluis@gmail.com>
2017-05-09 11:20:13 -0500

    Small example on how to make duplication a little more obvious.

--- src/CodeupClassroom.java
+++ src/CodeupClassroom.java
@@ -1,22 +1,28 @@
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class CodeupClassroom {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        PrintStream output = System.out; // Extracted a variable
+        double width;
+        double length;
 
         // input
-        System.out.println("Enter the width of the classroom");
-        double width = input.nextDouble();
+        // promptDouble(message)
+        output.println("Enter the width of the classroom");
+        width = input.nextDouble();
 
-        System.out.println("Enter the length of the classroom");
-        double length = input.nextDouble();
+        // promptDouble(message)
+        output.println("Enter the length of the classroom");
+        length = input.nextDouble();
 
         // process
         double area = length * width;
         double perimeter = 2 * length + 2 * width;
 
         // output
-        System.out.printf("The area of the classroom is %.2f%n", area);
-        System.out.printf("The perimeter of the classroom is %.2f\n", perimeter);
+        output.printf("The area of the classroom is %.2f%n", area);
+        output.printf("The perimeter of the classroom is %.2f\n", perimeter);
     }
 }
