9279c698a94dbb7eac3eeda1ec3949a5198eb3cb
Luis Montealegre <montealegreluis@gmail.com>
2017-05-10 11:38:56 -0500

    Solve the first exercise of part 1.

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,9 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 15) { // continue as long as i is less than or equals to 15
+            System.out.println(i);
+            i++;
+        }
+    }
+}
