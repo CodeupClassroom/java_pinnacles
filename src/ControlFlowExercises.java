25acd1dd2d8ad0241fd50520038c338e478637bd
Luis Montealegre <montealegreluis@gmail.com>
2017-05-10 11:45:05 -0500

    Completed second exercise with a do-while

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,17 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        // i
         int i = 5;
         while (i <= 15) { // continue as long as i is less than or equals to 15
             System.out.println(i);
             i++;
         }
+
+        // ii
+        int count = 0;
+        do {
+            System.out.println(count);
+            count += 2;
+        } while (count <= 100);
     }
 }
