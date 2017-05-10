8780503e217f24ae1b545de5fad718f1b219b435
Luis Montealegre <montealegreluis@gmail.com>
2017-05-10 11:53:04 -0500

    Completed refactoring of the 2 previous exercise to use a for loop.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,17 +1,23 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
         // i
-        int i = 5;
-        while (i <= 15) { // continue as long as i is less than or equals to 15
+//        int i = 5;
+//        while (i <= 15) { // continue as long as i is less than or equals to 15
+//            System.out.println(i);
+//            i++;
+//        }
+        for (int i = 5; i <= 15; i++) {
             System.out.println(i);
-            i++;
         }
 
         // ii
-        int count = 0;
-        do {
+//        int count = 0;
+//        do {
+//            System.out.println(count);
+//            count += 2;
+//        } while (count <= 100);
+        for (int count = 0; count <= 100; count += 2) {
             System.out.println(count);
-            count += 2;
-        } while (count <= 100);
+        }
     }
 }
