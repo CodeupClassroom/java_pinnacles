0d654e7cbb3386ad969447ee6381f1ccad9f9207
Luis Montealegre <montealegreluis@gmail.com>
2017-05-15 11:19:05 -0500

    Make the input number a user input value.

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 public class Factorial {
     public static void main(String[] args) {
         // input is a number (integer)
         // factorial is the output (integer -> long)
-        long number = 7;
-        long factorial = 1;
+        long number;
+        long factorial;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an integer number");
+        number = input.nextLong();
 
         // there's gonna be a series of transformations
         // constant -> variable
         // variable -> if statement
         // if -> loop
+        factorial = 1;
         for (int i = 1; i <= number; i++) { // counter
             factorial = factorial * i;  // accumulator
         }
