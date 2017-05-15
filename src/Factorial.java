ae0c7f4f49f9fcad9d428b753b10d0cb84f4c0d1
Luis Montealegre <montealegreluis@gmail.com>
2017-05-15 11:33:02 -0500

    Ask the user if she wants to calculate another factorial.

--- src/Factorial.java
+++ src/Factorial.java
@@ -8,18 +8,26 @@ public class Factorial {
         long factorial;
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter an integer number");
-        number = input.nextLong();
+        String wantsToContinue;
+        do {
+            System.out.println("Enter an integer number");
+            number = input.nextLong();
 
-        // there's gonna be a series of transformations
-        // constant -> variable
-        // variable -> if statement
-        // if -> loop
-        factorial = 1;
-        for (int i = 1; i <= number; i++) { // counter
-            factorial = factorial * i;  // accumulator
-        }
+            // there's gonna be a series of transformations
+            // constant -> variable
+            // variable -> if statement
+            // if -> loop
+            factorial = 1;
+            for (int i = 1; i <= number; i++) { // counter
+                factorial = factorial * i;  // accumulator
+            }
 
-        System.out.println(number + "! = " + factorial);
+            System.out.println(number + "! = " + factorial);
+
+            System.out.println("Do you want to continue (y/n)?");
+            // delimiter, delimiter for next is the space
+            // delimiter for nextLine is the enter
+            wantsToContinue = input.next();
+        } while ("y".equalsIgnoreCase(wantsToContinue));
     }
 }
