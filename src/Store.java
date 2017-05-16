a9bcbfcce62c0d74dda0f58fc975a6c21fd935ea
Luis Montealegre <montealegreluis@gmail.com>
2017-05-16 12:28:40 -0500

    Implement receipt option

--- src/Store.java
+++ src/Store.java
@@ -17,26 +17,43 @@ public class Store {
         String receipt = ""; // accumulator
         String wantsToContinue;
         double productTotal;
-        do {
-            System.out.println("Enter the name of the product");
-            String productDescription = input.nextLine();
-            System.out.println("Enter the price of the product");
-            double productPrice = input.nextDouble();
-            System.out.println("Enter the quantity that you're buying");
-            int quantity = input.nextInt();
-            // Remove trailing enter, it would produce an error before reading the next product description
-            input.nextLine();
+        double total = 0; // accumulator
 
-            productTotal = productPrice * quantity;
-            String receiptLine = String.format(
-                "%s $%.2f %d $%.2f\n",
-                productDescription, productPrice, quantity, productTotal
-            );
-            receipt += receiptLine;
-            System.out.println(receipt);
+        System.out.println("1. Sale\n2. Receipt\n3. Exit");
+        int option = input.nextInt();
+        input.nextLine();
+        switch (option) {
+            case 1:
+                do {
+                    System.out.println("Enter the name of the product");
+                    String productDescription = input.nextLine();
+                    System.out.println("Enter the price of the product");
+                    double productPrice = input.nextDouble();
+                    System.out.println("Enter the quantity that you're buying");
+                    int quantity = input.nextInt();
+                    // Remove trailing enter, it would produce an error before reading the next product description
+                    input.nextLine();
 
-            System.out.println("Do you want to buy another product? (y/n)");
-            wantsToContinue = input.nextLine();
-        } while ("y".equalsIgnoreCase(wantsToContinue));
+                    productTotal = productPrice * quantity;
+                    String receiptLine = String.format(
+                        "%s $%.2f %d $%.2f\n",
+                        productDescription, productPrice, quantity, productTotal
+                    );
+
+                    receipt += receiptLine;
+                    total =+ productTotal;
+
+                    System.out.println("Do you want to buy another product? (y/n)");
+                    wantsToContinue = input.nextLine();
+                } while ("y".equalsIgnoreCase(wantsToContinue));
+                break;
+            case 2:
+                System.out.println(receipt);
+                System.out.printf("Total is: $%.2f", total);
+                break;
+        }
+
+
+
     }
 }
