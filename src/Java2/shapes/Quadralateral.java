bbfc7bc3a6acd7d8b737bce875b05e96a01d94ba
Luis Montealegre <montealegreluis@gmail.com>
2017-05-22 09:38:31 -0500

    Add properties, getters and setters to Quadralateral

--- src/Java2/shapes/Quadralateral.java
+++ src/Java2/shapes/Quadralateral.java
@@ -4,4 +4,23 @@
 package Java2.shapes;
 
 abstract public class Quadralateral  extends Shape {
+    private final double length;
+    private final double width;
+
+    public Quadralateral(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    abstract public void setLength(double length);
+
+    abstract public void setWidth(double width);
 }
