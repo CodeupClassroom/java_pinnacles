23feedf42673dd9c13411529d33c2c322e39092a
Luis Montealegre <montealegreluis@gmail.com>
2017-05-22 09:58:08 -0500

    Verify that both Square and Rectangle are Measurable Shapes.

--- src/Java2/shapes/ShapeTest.java
+++ src/Java2/shapes/ShapeTest.java
@@ -2,13 +2,14 @@ package Java2.shapes;
 
 public class ShapeTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(4, 5);
+        Measurable myShape;
 
-        System.out.println(box1.getPerimeter());
-        System.out.println(box1.getArea());
+        myShape = new Rectangle(5.5, 3.0);
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
 
-        Rectangle box2 = new Square(5);
-        System.out.println(box2.getPerimeter());
-        System.out.println(box2.getArea());
+        myShape = new Square(3.0);
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
     }
 }
