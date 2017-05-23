1123e9e4ead857ce4568a3e55808648a95bf2a98
Luis Montealegre <montealegreluis@gmail.com>
2017-05-23 10:38:56 -0500

    Add variables and methods to the Student class.

--- src/students/Student.java
+++ src/students/Student.java
@@ -3,5 +3,33 @@
  */
 package students;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
+    private String name;
+    private List<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        grades = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // adds the given grade to the grades property
+    public void addGrade(int grade) {
+        grades.add(grade);
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage() {
+        int sum = 0;
+        for (Integer grade : grades) {
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
 }
