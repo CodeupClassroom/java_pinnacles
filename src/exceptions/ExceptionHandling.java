d7cf1dc6b0a6446a288184a294eb904b4f192712
Luis Montealegre <montealegreluis@gmail.com>
2017-05-24 11:44:36 -0500

    Create examples of exception handling
    * Difference between checked and unchecked exceptions
    * Try/catching exceptions
    * Adding the exception to the method signature
    * Throwing exceptions
    * Create and use custom exceptions

--- src/exceptions/ExceptionHandling.java
+++ src/exceptions/ExceptionHandling.java
@@ -4,4 +4,26 @@
 package exceptions;
 
 public class ExceptionHandling {
+    public static void main(String[] args) {
+        Account myAccount;
+        try {
+            System.out.println(56 / 60);
+            //throw new Exception("Something went wrong"); // stop the execution of the code
+            myAccount = new Account(12);
+            myAccount.withdrawal(-100);
+
+            System.out.println(myAccount.getBalance());
+        } catch (ArithmeticException exception) {
+            System.out.println(exception.getMessage());
+            // log this message
+        } catch (InitialNegativeBalance exception) {
+            System.out.println(exception.getMessage());
+            // send an email to a supervisor
+        } catch (WithdrawNegativeAttempt e) {
+            System.out.println(e.getMessage());
+            // suspend temporarily this account
+        } catch (Exception e) {   // Pokemon exception handling
+            System.out.println(e.getMessage());
+        }
+    }
 }
